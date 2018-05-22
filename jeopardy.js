1f221d5943e7cc7bf107d4a5e9d8f438b7b4e00a
jmwofford <jmwofford@gmail.com>
2018-05-22 13:20:58 -0400

    Lost in the World

--- jeopardy.js
+++ jeopardy.js
@@ -1,102 +1,90 @@
-alert("Welcome to Jeopardy");
-alert("Directions: Pick a category and a point value. Click on the chosen box for the question. Students must give the answer in the form of a question before clicking again. The teacher may want to set a time limit for answering the question. To see if a student or group is correct, click again for the answer. Click the “Back to Board” button on the slide to return to the main board. If the student or team is correct, they are awarded the point value of the question. (Click the “Score” button located on the main board to add the point value to the appropriate team score.The dollar values disappear after each question. Continue until all questions have been answered. The team with the most points wins.");
+// alert("Welcome to Jeopardy");
+// alert("Directions: Pick a category and a point value. Click on the chosen box for the question. Students must give the answer in the form of a question before clicking again. The teacher may want to set a time limit for answering the question. To see if a student or group is correct, click again for the answer. Click the “Back to Board” button on the slide to return to the main board. If the student or team is correct, they are awarded the point value of the question. (Click the “Score” button located on the main board to add the point value to the appropriate team score.The dollar values disappear after each question. Continue until all questions have been answered. The team with the most points wins.");
 
 
 
-// var score= 
-
-// var rightAnswer = 
-// var wrongAnswer =
+// var score;
 
 
-// pointvalue=
+// var wrongAnswer = function () {
 
-
-
-
-// All slots on board as object
-// const boardInfo = {
-//     q100: {
-//         question: "someting",
-//         answer: "something else",
-//         points: 200
-//     },
-//     q200: {
-//         question: "someting",
-//         answer: "something else",
-//         points: 200
+//     if (wrongAnswer) {
+//         PointVal - score;
 //     }
 
-
 // }
-//Object that will contain Question, Answer, and Point Value.
+
+// var rightAnswer = function () {
+//     if (rightAnswer) {
+//         PointVal + score;
+//     }
+// }
+
+
+// const questions = [{
+//     question: 'test question',
+//     answer: 'test answer',
+//     id: 'q100',
+//     pointVal: 100
+// }]
+
+const index1 = [{
+    question: 'entertainment question',
+    answer: 'entertainment answer',
+    id: 'q100',
+    pointVal: 100
+}]
+const index2 = [{
+    question: 'geography question',
+    answer: 'geography answer',
+    id: 'q100',
+    pointVal: 100
+}]
+const index3 = [{
+    question: 'math question',
+    answer: 'math answer',
+    id: 'q100',
+    pointVal: 100
+}]
+const index4 = [{
+    question: 'music question',
+    answer: 'music answer',
+    id: 'q100',
+    pointVal: 100
+}]
+const index5 = [{
+    question: 'art question',
+    answer: 'art answer',
+    id: 'q100',
+    pointVal: 100
+}]
 
 
 //   const currentQuestionInfo = boardInfo['q100'];
 
 
 
-$('.q100').click(function(){
+$('.q100').click(function () {
+    const question = index1.find((question) => {
+        return index1.id === 'q100'
+    })
 
-  
-$(this).html("Question is ... blahblahblah")
+    
+    $(this).html("Question is ... blahblahblah")
 
-$(this).prepend('<img id="clock" src="timer30.gif" />') 
+    $(this).prepend('<img id="clock" src="timer30.gif" />')
 
- $(this).css({ position: 'absolute', height:'400px', width:'600px', background:'rgba(0,0,0,.9)'})
+    $(this).css({ position: 'absolute', height: '600px', width: '600px', background: 'rgba(0,0,0,.9)' })
 
- $(this).append('<audio autoplay src="jeopardytheme.mp3" type="audio/mp3">');
-
-$(this).blur();
+    $(this).append('<audio autoplay src="jeopardytheme.mp3" type="audio/mp3">');
+    $(this).append('<button class="isright">RightAnswer</button')
+    $(this).append('<button class="isright">WrongAnswer</button')
+    $(this).blur();
 });
 
-
-
-// function toggleQuestion(questionId, questionText, correctAnswer){
-//     // toggle the CSS
-//  const displayQuestion = ('q100', currentQuestionInfo.question, currentQuestionInfo.answer);
+$("isright").click(function () {
 
 
 
+})
 
-
-
-
-
-
-
-    // if (this.height == '50px' && this.width == '100px'){
-
-    //      const unclickedBox = (this);
-
-    //      
-
-    //      //change text to Question
-
-    //     
-            //make "questionBox" pop up
-
-
-    //     $(unclickedBox).keydown(function(){
-
-        
-    
-    
-    
-    // )} else{ 
-    //     const clickedBox = (this);
-    // }
-        // If is small, make big and populate with question + buttons
-        // If large, make small and clean up text
-    // display question text
-    
-  
-
-            
-    // }
-
-    // user enters answer
-
-    // check provided answer with correct answer
-
-// }
